1d00d768f609cb09cd76f9b769c7b85fdbd4ba49
Vedant Pawase <vedantpawase21@gmail.com>
2026-09-09 23:13:27 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -12,14 +12,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os.path
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-8ksqxyzz4e^qw1ol@qd&@j)d5#%9brmdeo)81wmx%j0gl@id+d'
@@ -45,7 +44,7 @@ TEMPLATES = [
         },
     },
 ]
-# Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -86,8 +85,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'growth_tracker.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -116,8 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
+
 
 LANGUAGE_CODE = 'en-us'
 
@@ -128,12 +124,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
