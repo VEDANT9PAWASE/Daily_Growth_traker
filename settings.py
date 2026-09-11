4c2c7f37fea73879e2ccacbb84bd71f9e8b21593
Vedant Pawase <vedantpawase21@gmail.com>
2026-09-11 17:19:27 +0530

    Restore Django settings with initial configuration

--- settings.py
+++ settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # THIS LINE BELOW IS THE KEY:
+      
         'DIRS': [BASE_DIR / 'templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
